56da493690735ac079e0f12490adf0e06447d21e
Culganosi <culgan_@hotmail.com>
2022-03-16 15:40:27 -0400

    Implented WASD movement

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,9 +12,25 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function (data) {
+const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w') {
+    console.log('up')
+    connection.write('Move: up')
+  }
+  if (key === 'a') {
+    console.log('left')
+    connection.write('Move: left')
+  }
+  if (key === 's') {
+    console.log('down')
+    connection.write('Move: down')
+  }
+  if (key === 'd') {
+    console.log('right')
+    connection.write('Move: right')
   }
 };
 
