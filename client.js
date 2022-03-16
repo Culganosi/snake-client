7062817bd62b00b1f12f2f361125e68dac2d86ee
Culganosi <culgan_@hotmail.com>
2022-03-16 14:13:42 -0400

    added on connect message && send name to server

--- client.js
+++ client.js
@@ -8,6 +8,11 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server")
+    conn.write("Name: AJJ")
+  })
+
   conn.on('data', (idleMessage)=> {
     console.log(idleMessage);
   });
